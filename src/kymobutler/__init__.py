faba921add23134d6075f1ebc030c23f13f40c72
Craig Russell <craig.russell.phd@gmail.com>
2026-03-02 13:10:59 +0000

    Wire exported API and add PyWavelets dependency

--- src/kymobutler/__init__.py
+++ src/kymobutler/__init__.py
@@ -1,1 +1,55 @@
+"""Python port of KymoButler."""
+
+from .core import (
+    benchmark_prediction,
+    bi_kymobutler,
+    bi_kymobutler_segment,
+    bi_kymobutler_track,
+    load_default_nets,
+    uni_kymobutler,
+    uni_kymobutler_segment,
+    uni_kymobutler_track,
+)
+from .models import (
+    ClassNet,
+    UNet,
+    UNetUnidirectional,
+    VisionModule,
+    build_classnet,
+    build_unet,
+    build_unet_dsw,
+    build_unet_dsw_unidirectional,
+    build_unet_unidirectional,
+    build_vision_module,
+)
+from .postprocess import get_derived_quantities, pproc, pproc_local
+from .wavelets import analyse_kymograph_bi_wavelet, stationary_wavelet_sum, wavelet_segment
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "ClassNet",
+    "UNet",
+    "UNetUnidirectional",
+    "VisionModule",
+    "analyse_kymograph_bi_wavelet",
+    "benchmark_prediction",
+    "bi_kymobutler",
+    "bi_kymobutler_segment",
+    "bi_kymobutler_track",
+    "build_classnet",
+    "build_unet",
+    "build_unet_dsw",
+    "build_unet_dsw_unidirectional",
+    "build_unet_unidirectional",
+    "build_vision_module",
+    "get_derived_quantities",
+    "load_default_nets",
+    "pproc",
+    "pproc_local",
+    "stationary_wavelet_sum",
+    "uni_kymobutler",
+    "uni_kymobutler_segment",
+    "uni_kymobutler_track",
+    "wavelet_segment",
+]
